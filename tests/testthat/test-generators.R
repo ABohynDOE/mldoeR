7a0556d2bd49549388e2e0b8746c6e51b07c0251
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2021-08-13 12:28:16 +0200

    Add test for char2num

--- tests/testthat/test-generators.R
+++ tests/testthat/test-generators.R
@@ -1,14 +1,24 @@
-test_that("Generator char to num works", {
+test_that("num2char works", {
   expect_equal(num2char(15), "abcd")
 })
 
-test_that("Generator num to vector works", {
+test_that("num2vec works", {
   expect_equal(num2vec(7), c(1, 1, 1))
   expect_equal(num2vec(13, 5), c(1, 0, 1, 1, 0))
 })
 
-test_that("Generators functions do not work with x < 1", {
+test_that("char2num works", {
+  # Normal test
+  expect_equal(char2num("abcd"), 15)
+  # Test with wrong generator
+  expect_equal(char2num("AaCCCcb"), 7)
+})
+
+test_that("Generators functions only take x > 1", {
+  # No negative number input
   expect_error(num2char(-1), "x must be a positive integer")
   expect_error(num2vec(-1), "x must be a positive integer")
-  expect_error(num2vec(-1, 4), "x must be a positive integer")
+  # No empty string or non-string
+  expect_error(char2num(''), "s must be a non-empty string")
+  expect_error(char2num(15), "s must be a string")
 })

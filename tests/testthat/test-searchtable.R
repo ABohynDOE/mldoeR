d2c555384f92bf2594b2b419d59eb7ca96015d46
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2021-08-13 12:57:09 +0200

    Two-level search-table test

--- tests/testthat/test-searchtable.R
+++ tests/testthat/test-searchtable.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Search-table has correct size and type", {
+  # Define k, p and R
+  k = 4
+  p = 2
+  R = 3
+  st <- searchtable(k,p,R)
+  # Test type of the search-table
+  expect_type(st, "list")
+  # Test the size
+  ncols = p+1
+  nrows = 2^k-1-k
+  expect_equal(dim(st), c(nrows,ncols))
 })
